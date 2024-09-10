72e1a521521d8e0f42e5ff2cc0577919c759b6e9
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-09-10 11:13:00 +0300

    feat: reworked

--- prog/lab1/src/Main.java
+++ prog/lab1/src/Main.java
@@ -1,5 +1,3 @@
-import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.random.RandomGenerator;
 
 public class Main {
@@ -14,46 +12,41 @@ public class Main {
         }
     }
 
-    private static String formatArr(double[][] arr) {
-        String[][] tmp = new String[16][11];
-
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 11; j++) {
-                tmp[i][j] = new DecimalFormat("##.####").format(arr[i][j]);
-            }
+    private static void calculateW(double[][] w, short[] s, double[] x, int i, int j) {
+        switch (s[i]) {
+            case 15:
+                w[i][j] = Math.log10(Math.sqrt(Math.pow((Math.abs(x[j]) + 1) / 2, 2)));
+                break;
+            case 1, 3, 7, 8, 11, 12, 13, 16:
+                w[i][j] = Math.pow(((double) 3 / 4 * ((Math.pow((Math.cbrt(x[j])), 3) / ((double) 1 / 3 + Math.pow(x[j], x[j] + Math.PI))) + 1)), 3);
+                break;
+            default:
+                w[i][j] = Math.tan(Math.pow(Math.tan(((double) 1 / 2) / x[j]), Math.pow((double) 2 / 3 * Math.tan(x[j]), 2)));
         }
-        return Arrays.deepToString(tmp);
+    }
+
+    private static void printFormatted(double[][] w) {
+        for (double[] row : w) {
+            for (double e : row) {
+                System.out.format("%8.4f ", e);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        short[] r = new short[16];
+        short[] s = new short[16];
         double[] x = new double[11];
         w = new double[16][11];
 
-        generateRX(r, x);
+        generateRX(s, x);
 
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 11; j++) {
-                switch (r[i]) {
-                    case 15:
-                        w[i][j] = Math.log10(
-                                Math.sqrt(
-                                        Math.pow(
-                                                (Math.abs(x[j]) + 1) / 2
-                                                , 2)
-                                )
-                        );
-                        break;
-                    case 1, 3, 7, 8, 11, 12, 13, 16:
-                        w[i][j] = Math.pow(((double) 3 / 4 * ((Math.pow((Math.cbrt(x[j])), 3) / ((double) 1 / 3 + Math.pow(x[j], x[j] + Math.PI))) + 1)), 3);
-                        break;
-                    default:
-                        w[i][j] = Math.tan(Math.pow(Math.tan(((double) 1 / 2) / x[j]), Math.pow((double) 2 / 3 * Math.tan(x[j]), 2)));
-                        break;
-                }
+                calculateW(w, s, x, i, j);
             }
         }
 
-        System.out.println(formatArr(w));
+        printFormatted(w);
     }
 }
