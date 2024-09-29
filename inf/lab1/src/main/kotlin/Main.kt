f4266bd01f3d03c07a7ec4f23a262260295a2bcf
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-09-30 02:40:42 +0300

    feat: first lab of inf is done
    nah bro im too lazy i should work harder

--- inf/lab1/src/main/kotlin/Main.kt
+++ inf/lab1/src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package ru.pozitp
 
-fun main() {
-    println("Hello World!")
+fun convertFibToDec(str: String): Long {
+    var temp1 = 0
+    var temp2 = 1
+    var result: Long = 0
+    for (i in 1..str.length) {
+        result += temp1 * str[str.length - i].toString().toLong()
+
+        val sum = temp1 + temp2
+        temp1 = temp2
+        temp2 = sum
+    }
+
+    return result
+}
+
+fun main(array: Array<String>) {
+    if (array.isEmpty() || array.size > 1 || !Regex("[01]*").matches(array[0])) {
+        println("You must provide one number in fibonacci numerical system!")
+        return
+    }
+
+    println(convertFibToDec(array[0]))
 }
