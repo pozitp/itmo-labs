68f5ddf8f2c6e30ffba423917919e656096a24ab
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-10-07 12:27:16 +0300

    feat: changed opd

--- opd/lab1/lab1.sh
+++ opd/lab1/lab1.sh
@@ -47,10 +47,10 @@ chmod u-w ~/lab0/sandslash0/aipom
 ln -f ~/lab0/igglybuff7 ~/lab0/mudkip3/mareepigglybuff
 
 chmod u+r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
-chmod u+x ~/lab0/mudkip3
+chmod u+x ~/lab0/leafon5
 wc -m ~/lab0/mudkip3/* | tail -1 | grep -Go '[0-9]*' 2>> /tmp/error_375301.log > /tmp/wc_375301.log
 chmod u-r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
-chmod u-x ~/lab0/mudkip3
+chmod u-x ~/lab0/leafon5
 chmod u+r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
 ls -Rltr ~/lab0/ | grep "on" | tail -2 2> /dev/null
 chmod u-r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
