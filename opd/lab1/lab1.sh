89612d60c02d28c22271d0287920ddeed32110b7
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-10-08 09:25:43 +0300

    feat: changed opd

--- opd/lab1/lab1.sh
+++ opd/lab1/lab1.sh
@@ -67,8 +67,8 @@ chmod u+r ~/lab0/mudkip3
 find ~/lab0/mudkip3 -type f -name "ck" -exec sh -c "wc -m $1" -- {} \;
 chmod u-r ~/lab0/mudkip3
 
-find ~/lab0/ -type f -name "a*" -exec wc -m {} \; | sort -n  2>&1
-find ~/lab0/ -type f -name "p$" -exec cat {} \; | sort 2>> /tmp/error_375301.log 
+find ~/lab0/ -type f -name "a*" -exec wc -m {} + 2>&1 | sort -n
+find ~/lab0/ -type f -name "p$" -exec cat {} + 2>> /tmp/error_375301.log | sort 
 
 
 # rm ~/lab0/leafeon5
