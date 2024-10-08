3b3e13039ae3d7a448b76f3c437ee3a062fbe73c
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-10-08 09:50:31 +0300

    feat: changed opd

--- opd/lab1/lab1.sh
+++ opd/lab1/lab1.sh
@@ -106,9 +106,9 @@ echo "[TASK 4] Completed"
 # 5. Выполнить удаление файлов и каталогов при помощи команд rm и rmdir согласно варианту задания.
 
 # Удалить файл leafeon5
-rm ~/lab0/leafeon5
+rm -f ~/lab0/leafeon5
 # Удалить файл lab0/mudkip3/glaceon
-rm ~/lab0/mudkip3/glaceon
+rm -f ~/lab0/mudkip3/glaceon
 # удалить символические ссылки Copy_*
 rm ~/lab0/Copy_*
 # удалить жесткие ссылки lab0/mudkip3/mareepigglybu*
