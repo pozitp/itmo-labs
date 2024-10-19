b7b1ebadee07396e30113a2b0eb76ddfc674521a
Artur Mukhamedyarov <pozitp.code@gmail.com>
2024-10-19 15:53:19 +0300

    feat: changed opd

--- opd/lab1/lab1.sh
+++ opd/lab1/lab1.sh
@@ -14,8 +14,7 @@ echo $'Тип диеты\tErgovore' > ~/lab0/electrode3 && chmod u=rw,g=w,o= ~/l
 # igglybuff7: rw--w----
 echo $'Ходы\tBody Slam Bounce\nCounter Covet Double-Edge Endeavor Gravity Heal Bell Helping Hand\nHyper Voice Icy Wind Last Resort Magic Coat Mega Kick Mega Punch Mimic\nMud-Slap Pain Split Recycle Role Play Rollout Seismic Toss Shock Wave\nSleep Talk Snore Water Pulse Uproar' > ~/lab0/igglybuff7 && chmod 640 ~/lab0/igglybuff7
 echo $'Живет\tForest Grassland\nRainforest' > ~/lab0/leafeon5 && chmod u=r,go= ~/lab0/leafeon5 # leafeon5: r--------
-echo $'weigth=55.8 height=31.0 atk=7\ndef=4' > ~/lab0/mudkip3/accelgor && chmod 400 ~/lab0/mudkip3/accelgor # accelgor: r--------
-# psyduck: владелец должен читать и записывать файл; группа-владелец должна не иметь никаких прав; остальные пользователи должны не иметь никаких прав
+echo $'weigth=55.8 height=31.0 atk=7\ndef=4' > ~/lab0/mudkip3/accelgor && chmod 400 ~/lab0/mudkip3/accelgor # accelgor: r--------# psyduck: владелец должен читать и записывать файл; группа-владелец должна не иметь никаких прав; остальные пользователи должны не иметь никаких прав
 echo $'Способности\tTorrent Damp Cloud Nine' > ~/lab0/mudkip3/psyduck && chmod u=rw,go= ~/lab0/mudkip3/psyduck
 echo $'Живет\nTaiga Tundra' > ~/lab0/mudkip3/glaceon && chmod u=r,o=r,g= ~/lab0/mudkip3/glaceon # glaceon: r-----r--
 echo $'satk7 sdef=5 spd=4' > ~/lab0/mudkip3/mareep && chmod 006 ~/lab0/mudkip3/mareep # mareep: права 006
@@ -76,29 +75,38 @@ echo "[TASK 3] Completed"
 # 4. Используя команды cat, wc, ls, head, tail, echo, sort, grep выполнить в соответствии с вариантом задания поиск и фильтрацию файлов, каталогов и содержащихся в них данных.
 
 # Подсчитать количество символов содержимого файлов в директории mudkip3, результат записать в файл в директории /tmp, ошибки доступа перенаправить в файл в директории /tmp
-chmod u+r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
+#chmod u+r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
 wc -m ~/lab0/mudkip3/* 2>> /tmp/error_375301.log | tail -1 | grep -Go '[0-9]*' > /tmp/wc_375301.log
-chmod u-r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
+#chmod u-r ~/lab0/mudkip3 ~/lab0/mudkip3/mareep
 
 # Вывести два последних элемента рекурсивного списка имен и атрибутов файлов в директории lab0, содержащих строку "on", список отсортировать по убыванию даты доступа к файлу, подавить вывод ошибок доступа
-chmod u+r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
+#chmod u+r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
 ls -Rlt ~/lab0/ 2> /dev/null | grep "on" | tail -2 
-chmod u-r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
+#chmod u-r ~/lab0/weezing1/ducklett ~/lab0/mudkip3
 
 # Вывести содержимое файлов: psyduck, glaceon, mareep, zorua, исключить строки, заканчивающиеся на 'r', ошибки доступа перенаправить в файл в директории /tmp
-chmod u+r ~/lab0/mudkip3/mareep 
+#chmod u+r ~/lab0/mudkip3/mareep 
 cat ~/lab0/mudkip3/psyduck ~/lab0/mudkip3/glaceon ~/lab0/mudkip3/mareep ~/lab0/mudkip3/zorua 2>> /tmp/error_375301.log | grep -v 'r$' 
-chmod u-r ~/lab0/mudkip3/mareep
+#chmod u-r ~/lab0/mudkip3/mareep
 
 # Вывести содержимое файлов с номерами строк в директории mudkip3, оставить только строки, содержащие "ck", регистр символов игнорировать, ошибки доступа не подавлять и не перенаправлять
-chmod u+r ~/lab0/mudkip3
-find ~/lab0/mudkip3 -type f -name "ck" -exec sh -c "wc -m $1" -- {} \;
-chmod u-r ~/lab0/mudkip3
+#chmod u+r ~/lab0/mudkip3
+cat -n ~/lab0/mudkip3/* | grep -i "ck"
+#chmod u-r ~/lab0/mudkip3
 
 # Рекурсивно подсчитать количество символов содержимого файлов из директории lab0, имя которых начинается на 'a', отсортировать вывод по увеличению количества, добавить вывод ошибок доступа в стандартный поток вывода
-find ~/lab0/ -type f -name "a*" -exec wc -m {} + 2>&1 | sort -n
+for file in ~/lab0/**/a*; do
+  if [[ -f "$file" ]]; then
+    wc -m "$file" 2>&1
+  fi
+done | sort -n
+
 # Рекурсивно вывести содержимое файлов из директории lab0, имя которых заканчивается на 'p', строки отсортировать по имени a->z, ошибки доступа перенаправить в файл в директории /tmp
-find ~/lab0/ -type f -name "p$" -exec cat {} + 2>> /tmp/error_375301.log | sort 
+for file in ~/lab0/**/*p; do
+  if [[ -f "$file" ]]; then
+    cat "$file" 2>> /tmp/error_375301.log
+  fi
+done | sort
 
 echo "[TASK 4] Completed"
 
@@ -106,17 +114,17 @@ echo "[TASK 4] Completed"
 # 5. Выполнить удаление файлов и каталогов при помощи команд rm и rmdir согласно варианту задания.
 
 # Удалить файл leafeon5
-rm -f ~/lab0/leafeon5
+#rm -f ~/lab0/leafeon5
 # Удалить файл lab0/mudkip3/glaceon
-rm -f ~/lab0/mudkip3/glaceon
+#rm -f ~/lab0/mudkip3/glaceon
 # удалить символические ссылки Copy_*
-rm ~/lab0/Copy_*
+#rm ~/lab0/Copy_*
 # удалить жесткие ссылки lab0/mudkip3/mareepigglybu*
-find ~/lab0/mudkip3/ -name "mareepigglybu*" -exec rm {} \;
+#rm -f ~/lab0/mudkip3/mareepigglybu*
 # Удалить директорию weezing1
-chmod -R u+w ~/lab0/weezing1
-rm -r ~/lab0/weezing1
+#chmod -R u+w ~/lab0/weezing1
+#rm -r ~/lab0/weezing1
 # Удалить директорию lab0/weezing1/larvesta
-rm -r ~/lab0/weezing1/larvesta
+#rm -r ~/lab0/weezing1/larvesta
 
 echo "[TASK 5] Completed"
